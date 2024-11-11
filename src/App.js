4362026c882adb2df73f3c5a5b3de7354531a9f2
mkndrr <masha.kondratova0209@gmail.com>
2024-11-11 21:39:48 +0300

    add bags(array)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React from 'react';
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Items from './Components/Items';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Сумка Balenciaga Rodeo Medium',
+          img: '',
+          desc: '',
+          category: 'tote',
+          price: '8990'
+        },
+        {
+          id: 2,
+          title: 'Сумка Saint Laurent Loulou',
+          img: '',
+          desc: '',
+          category: 'crossbody',
+          price: '6490'
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className='wrapper'>
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
